269ca966827221e0526ad3142c749e0bc7b8926f
uk6342 <uk6342@gmail.com>
2017-08-09 08:58:57 +0900

    해먹남녀 크롤링, 제목 그림 url

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -15,59 +15,32 @@ class RecipesController < ApplicationController
       render json: @recipe.to_json(only: [:id, :title, :url, :image])
     else
       data = Nokogiri::HTML(open(url))
-      title = data.css('.view2_summary h3').text
-      image = data.css('.centeredcrop img').attr('src')
-      @material = get_material(data)
-      @recipe = Recipe.new(title: title, url: url, image: image)
-      if @recipe.save!
-        render json: {recipe: @recipe.to_json(only: [:title, :url, :image]),
-                      materials: @material.to_json
-        }
+      if !data.nil?
+        title = data.css('div.aside h1 strong').text
+        image = data.css('ul.slides li img').first.attr('src')
+
+        # for material
+        #data.css("div.btm li").each do |li|
+          #name = li.css('span').text.delete(' ')
+          #un_unit = li.css('em').text.delete(' ')
+          #unit = un_unit.split(/(?<=\d)(?=[ㄱ-ㅎ|가-힣|a-z|A-Z|])/).last
+          #if !un_unit.nil? && !unit.nil?
+            #name = name.split("(").first
+            #unit = unit.split("(").first
+            ##puts "#{title} #{name} >> #{unit}"
+          #end
+        #end
+
+        @recipe = Recipe.new(title: title, url: url, image: image)
+        if @recipe.save!
+          render json: @recipe.to_json(only: [:title, :url, :image])
+        else
+          render status: "unpermitted url"
+        end
       else
         render status: "unpermitted url"
       end
     end
-  end
-
-  def get_material(input)
-    material = Hash.new()
-    case1 = input.css('div.ready_ingre3 ul').count
-    case2 = input.css('div.cont_ingre').count
-    if case1 > 0
-      material = case1(input)
-    elsif case2 > 0
-      material = case2(input)
-    end
-    return material
-  end
-
-  ## 1 ul li 형식일때
-  def case1(input)
-    material = Hash.new()
-    input.css('div.ready_ingre3 ul').each do |u|
-      title = u.css('b').text
-      if title == "[재료]" || title == "[양념]" || title == "[소스]"
-        u.css('li').each do |li|
-          name = li.text.split("  ")[0]
-          unit = li.css('span.ingre_unit').text
-          material[name] = unit
-        end
-      end
-    end
-    return material
-  end
-
-  ## 2 ul li가 아니라 dl일때 !! 문제점이 있다!
-  def case2(input)
-    material = Hash.new()
-    input.css('div.cont_ingre dl').each do |dl|
-      dl.css('dd').text.split(',').each do |m|
-        title = m.split(' ').first
-        unit = m.split(' ').last
-        material[title] = unit
-      end
-    end
-    return material
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -14,49 +14,46 @@ require 'uri'
 
 #for product
 
-#def make_ourform(product_title)
-  #result = Hash.new
-  #name = ""
-  #weight = ""
-  #bundle = ""
-  #product_title.split(", ").each_with_index do |p, index|
-    #if index == 0
-      #name = p.strip
-    #elsif p.length > 5
-      #name = name + p.strip
-    #elsif p.include?("g") && p.exclude?("개")
-      #weight = p.strip
-    #else
-      #bundle = p.strip
-    #end
-  #end
-  #result["name"] = name
-  #result["weight"] = weight
-  #result["bundle"] = bundle
-  #return result
-#end
-
-#Material.last(540).each do |m|
-  #keyword = m.name
-  #url = "http://www.coupang.com/np/search?component=194176&q=" + URI.encode(keyword)
-  #data = Nokogiri::HTML(open(url))
-  #if data.nil?
-    #product = data.css('li.search-product a dl').first
-    #if product.css('dd.descriptions div.price-area strong.price-value').first.nil?
-      #product = data.css('li.search-product a dl').second
-    #end
-    #name = product.css('dd.descriptions div.name').text
-    #description =  make_ourform(name)
-    #price = product.css('dd.descriptions div.price-area strong.price-value').first.text
-    #image = product.css('dt.image img').attr('src').text.delete('//')
-    #product = Product.new(price: price, image: image, name: description["name"],
-                          #weight: description["weight"], bundle: description["bundle"],
-                          #material_id: m.id)
-    #if product.save!
-      #puts "#{product.name} is created"
-    #end
-  #end
-#end
+def make_ourform(product_title)
+  result = Hash.new
+  name = ""
+  weight = ""
+  bundle = ""
+  product_title.split(", ").each_with_index do |p, index|
+    if index == 0
+      name = p.strip
+    elsif p.length > 5
+      name = name + p.strip
+    elsif p.include?("g") && p.exclude?("개")
+      weight = p.strip
+    else
+      bundle = p.strip
+    end
+  end
+  result["name"] = name
+  result["weight"] = weight
+  result["bundle"] = bundle
+  return result
+end Material.last(545).each do |m| keyword = m.name
+  url = "http://www.coupang.com/np/search?component=194176&q=" + URI.encode(keyword)
+  data = Nokogiri::HTML(open(url))
+  if data.nil?
+    product = data.css('li.search-product a dl').first
+    if product.css('dd.descriptions div.price-area strong.price-value').first.nil?
+      product = data.css('li.search-product a dl').second
+    end
+    name = product.css('dd.descriptions div.name').text
+    description =  make_ourform(name)
+    price = product.css('dd.descriptions div.price-area strong.price-value').first.text
+    image = product.css('dt.image img').attr('src').text.delete('//')
+    product = Product.new(price: price, image: image, name: description["name"],
+                          weight: description["weight"], bundle: description["bundle"],
+                          material_id: m.id)
+    if product.save!
+      puts "#{product.name} is created"
+    end
+  end
+end
 
 
 # for recipe crawilng
