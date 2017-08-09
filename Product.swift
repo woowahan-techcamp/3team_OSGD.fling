34f0fa95a97ca12c552836796b8437c5182d6513
Yerin Hwang <yrtech@woowahan.com>
2017-08-09 14:59:18 +0900

    #4 add init function with product dictionary

--- Product.swift
+++ Product.swift
@@ -10,20 +10,34 @@ import Foundation
 
 class Product {
 
-    private let id: String
+    private let pid: String
     private let name: String
     private let price: Decimal
     private let weight: String
 
     init(pid: String, name: String, price: Decimal, weight: String) {
-        self.id = pid
+        self.pid = pid
+        self.name = name
+        self.price = price
+        self.weight = weight
+    }
+
+    init?(data: [String:Any]) {
+        guard let id = data["id"]! as? String,
+            let name = data["name"]! as? String,
+            let price = data["price"]! as? Decimal,
+            let weight = data["weight"]! as? String else {
+                return nil
+        }
+
+        self.pid = id
         self.name = name
         self.price = price
         self.weight = weight
     }
 
     func getId() -> String {
-        return self.id
+        return self.pid
     }
 
     func getPrice() -> Decimal {
