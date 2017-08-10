f6fad3fcdb4a5d74a86bca87a07ad4af80477ed2
asfaca <asfaca@naver.com>
2017-08-10 16:27:46 +0900

    추천메뉴 돔구조 조금 바꾸고 에이작스로 받아와서 이미지, 타이틀 넣었습니다.

--- src/main.js
+++ src/main.js
@@ -1,14 +1,16 @@
 document.addEventListener("DOMContentLoaded", (e) => {
-    
+    _.ajaxFunc("http://52.78.41.124/recipes", fillContentRecommendSection, ".recommend_content_list");
 })
 
 
+
+
 const _ = {
-    ajaxFunc : function ajax(url) {
+    ajaxFunc : function ajax(url, exeFunc, selector) {
     const oReq = new XMLHttpRequest(); 
         oReq.addEventListener("load", (e) => {
             const data = JSON.parse(oReq.responseText);
-            exeFunc();
+            exeFunc(data, selector);
         }); 
         oReq.open("GET", url); 
         oReq.send();
@@ -16,4 +18,25 @@ const _ = {
 }
 
 
+function fillContentRecommendSection(data, selector) {
+    const li =document.querySelector(selector).children;
+    
+    const liArr = Array.from(li);
+    let i = 0;
+    
+    liArr.forEach((e) => {
+        e.children[0].href = data[i].url;
+        e.children[0].children[0].style.backgroundImage = "url('" + data[i].image  + "')";
 
+        e.children[1].children[0].href = data[i].url;
+        ////
+        e.children[1].children[0].innerHTML = data[i].title;
+        /*
+        e.children[1].children[0].innerHTML = data[i].subtitle;
+        e.children[2].innerHTML = data[i].title + "  |  " + data[i].id;
+        */
+        i++;
+    })
+    
+
+}
