031e4215ee8046206a6718c9c8434d246f1aff2e
asfaca <asfaca@naver.com>
2017-08-10 14:30:58 +0900

    유틸리티 등 조금 수정

--- src/textBox.js
+++ src/textBox.js
@@ -1,7 +1,38 @@
-document.addEventListener("DOMContentLoaded", (e) => {
-    const a = new textBox(".search_form", "#search_bar_template", "레시피를 장바구니에 담아드립니다");
-})
+class textBox {
+    constructor(searchDivSelector) {
+        this.searchDivSelector = document.querySelector(searchDivSelector);
+        this.caretIcon = this.searchDivSelector.children[0];
+        this.searchText = this.searchDivSelector.children[1].children[0];
+        this.interval;
+        this.blinkCaret();
+        this.eventInit();
+    }
 
+    eventInit() {
+        this.searchText.addEventListener("keydown", (e) => {
+            window.clearInterval(this.interval);
+            this.caretIcon.style.opacity = 0;
+        });
+    }
+
+
+    blinkCaret() {
+        let turn = 0;
+
+        this.interval = window.setInterval(()=> { 
+            if (turn == 0) {
+                this.caretIcon.style.opacity = "0";
+                turn = 1;
+            }
+            else {
+                this.caretIcon.style.opacity = "1";
+                turn = 0;
+            }
+        },500);
+    }
+}
+
+//////////////////////////////////////////
 class textBox {
     constructor(searchDivSelector, templateSelector, placeHolder) {
         this.templateSelector = document.querySelector(templateSelector);
@@ -38,27 +69,4 @@ class textBox {
             }
         },500);
     }
-
-    template() {
-        const theTemplateScript = document.querySelector("#search_bar_template").innerHTML;
-        const theTemplate = Handlebars.compile(theTemplateScript);
-        const theCompiledHtml = theTemplate();
-        document.querySelector(this.searchDivSelector).innerHTML = theCompiledHtml;
-    }
 }
-
-
-const _ = {
-    ajaxFunc : function ajax(url) {
-    const oReq = new XMLHttpRequest(); 
-        oReq.addEventListener("load", (e) => {
-            const data = JSON.parse(oReq.responseText);
-            exeFunc();
-        }); 
-        oReq.open("GET", url); 
-        oReq.send();
-    }
-}
-
-
-
