f6133b0713cf1f2d68d247f86ee77346521e57b8
uk6342 <uk6342@gmail.com>
2017-08-10 14:20:59 +0900

    adder ACMO header

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,7 +3,17 @@ require 'json'
 
 require 'open-uri'
 class RecipesController < ApplicationController
-  def index
+
+  after_filter :cors_set_access_control_headers
+
+  def cors_set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
+    headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token'
+    headers['Access-Control-Max-Age'] = "1728000"
+  end
+
+  def  index
     @recipes = Recipe.last(6)
     render json: @recipes.to_json(only: [:id, :title, :url, :image])
   end
