c305742594f20d7ce85c1b6eb3fef1eadcf0fd0a
uk6342 <uk6342@gmail.com>
2017-08-11 10:19:55 +0900

    validate url in recipe controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -15,7 +15,17 @@ class RecipesController < ApplicationController
 
   def  index
     @recipes = Recipe.last(6)
-    render json: @recipes.to_json(only: [:id, :title, :url, :image])
+    render json: @recipes.to_json(only: [:id, :subtitle, :writer, :title, :url, :image])
+  end
+
+  def season
+    if Recipe.all.count < 9
+      render status: :no_content
+    else
+      @recipes = Recipe.last(6)
+      render json: @recipes.to_json(only: [:id, :subtitle, :writer, :title, :url, :image])
+    end
+
   end
 
   def get_products
@@ -30,42 +40,46 @@ class RecipesController < ApplicationController
   #search and create Recipe
   def create
     url = params[:url]
-    if Recipe.where(url: url).present?
-      @recipe = Recipe.where(url: url).first
-      render json: @recipe.to_json(only: [:id, :subtitle, :writer, :title, :url, :image])
-    else
-      data = Nokogiri::HTML(open(url))
-      if !data.nil?
-        if data.css('div.aside h1 strong').present?
-          title = data.css('div.aside h1 strong').text
-        else
-          title = ""
-        end
-        if data.css('ul.slides li img').first.present?
-          image = data.css('ul.slides li img').first.attr('src')
-        else
-          image = ""
-        end
-        if data.css('div.aside h1').present?
-          subtitle = data.css('div.aside h1').text.gsub(title, "").strip + " " + title
-        else
-          subtitle = ""
-        end
-        if data.css('strong.best a').present?
-          writer = data.css('strong.best a').text
-        else
-          writer = ""
-        end
-        if title.length != 0
-          @recipe = Recipe.create(title: title, subtitle: subtitle,
-                                  url: url, image: image, writer: writer)
-          render json: @recipe.to_json(only: [:id, :subtitle, :writer, :title, :url, :image])
-        else
-          render status: "unpermitted url"
-        end
+    if url.include?("http://haemukja.com/recipes/")
+      if Recipe.where(url: url).present?
+        @recipe = Recipe.where(url: url).first
+        render json: @recipe.to_json(only: [:id, :subtitle, :writer, :title, :url, :image])
       else
-        render status: "unpermitted url"
+        data = Nokogiri::HTML(open(url))
+        if !data.nil?
+          if data.css('div.aside h1 strong').present?
+            title = data.css('div.aside h1 strong').text
+          else
+            title = ""
+          end
+          if data.css('ul.slides li img').first.present?
+            image = data.css('ul.slides li img').first.attr('src')
+          else
+            image = ""
+          end
+          if data.css('div.aside h1').present?
+            subtitle = data.css('div.aside h1').text.gsub(title, "").strip + " " + title
+          else
+            subtitle = ""
+          end
+          if data.css('strong.best a').present?
+            writer = data.css('strong.best a').text
+          else
+            writer = "해먹남녀"
+          end
+          if title.length != 0
+            @recipe = Recipe.create(title: title, subtitle: subtitle,
+                                    url: url, image: image, writer: writer)
+            render json: @recipe.to_json(only: [:id, :subtitle, :writer, :title, :url, :image])
+          else
+            render status: :no_content
+          end
+        else
+          render status: :no_content
+        end
       end
+    else
+      render status: :no_content
     end
   end
 
