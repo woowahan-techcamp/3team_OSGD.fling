897a2386eb39e99c5dab6ff1e968c0492a9b8eda
Yerin Hwang <yrtech@woowahan.com>
2017-08-11 15:09:27 +0900

    #18 recipe - checkbox component source refactoring

--- flingTests/flingTests.swift
+++ flingTests/flingTests.swift
@@ -38,21 +38,4 @@ class FlingTests: XCTestCase {
 
         XCTAssertTrue(controller.checkRecipeUrl(url: "http://haemukja.com/recipes/49"))
     }
-
-    func testProduct() {
-        //swiftlint:disable line_length
-        let temp = [
-            ["id": 11, "material_id": 1, "name": "삼원농산 영양듬뿍 잡곡 귀리", "price": "5700", "weight": "5kg", "bundle": "1개", "image": "t5a.coupangcdn.comthumbnailsremote292x292eximageproductimagevendoritem201604193000580651036da747-80bb-40d8-9285-0f78f3dcaf50.jpg"
-            ],
-            ["id": 22, "material_id": 2, "name": "순수담 귀리볶음가루", "price": "10000", "weight": "1kg", "bundle": "1개", "image": "t1c.coupangcdn.comthumbnailsremote292x292eximageretailimages20160422182aab0317e-1cee-422b-bd46-9661393d3e17.JPG"
-            ],
-            ["id": 33, "material_id": 3, "name": "오뚜기 옛날 구수한 누룽지", "price": "14900", "weight": "3kg", "bundle": "1개", "image": "t2a.coupangcdn.comthumbnailsremote292x292eximageproductimagevendoritem201605173011864835d34b6acd-6756-4838-bee5-1094f3843f46.jpg"
-            ]
-        ]
-
-        let product = Product.init(data: temp[0])
-        let predict = temp[0]["name"] as? String
-
-        XCTAssertTrue(product?.getName() == predict)
-    }
 }
