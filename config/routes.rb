09ca5aded980df3f2e11e6d76b4a2c822d1d4b96
uk6342 <uk6342@gmail.com>
2017-08-16 16:40:56 +0900

    init products controller #23

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   resources :recipes
+  resources :products
   get '/get_products/:id', to: 'recipes#get_products'
   get '/season', to: 'recipes#season'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
