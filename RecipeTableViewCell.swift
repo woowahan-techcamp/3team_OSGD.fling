98e87818ceb5defece35a0abc51d4263485d10cb
sdu6342 <uk6342@gmail.com>
2017-08-22 10:56:30 +0900

    fix table scroll bug #34

--- RecipeTableViewCell.swift
+++ RecipeTableViewCell.swift
@@ -47,7 +47,6 @@ class RecipeTableViewCell: UITableViewCell {
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
         if selected {
             self.disclosureHandler()
         }
