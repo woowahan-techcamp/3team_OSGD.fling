6ae6adcdaab6df2c8fe86fbb77a61fdb3bb5be1f
chsbru <chsbru@gmail.com>
2017-08-23 18:00:55 +0900

    메인 리팩토링 진행중

--- src/main.js
+++ src/main.js
@@ -1,7 +1,105 @@
+var Fling = {};
+Fling.data = {
+    apiBaseURL: 'http://52.79.119.41'
+}
+
+Fling.template = {
+    'tileView': '<div class...>'
+}
+var view = new Fling.View()
+view.data = '1'
+Fling.View = class {
+    constructor() {
+        this.dataObject = null;
+    }
+    get data() {
+        return this.dataObject;
+    }
+    set data(obj) {
+        this.dataObject = obj;
+        return;
+    }
+    get render() {
+        return `
+        <div class="recommend_content_wrap">
+            <ul class="recommend_content_list">
+                <li>
+                    <a href="">
+                        <div class="recommend_content_list_node">
+                            <div class="recommend_number"><p>1</p></div>
+                        </div>
+                    </a>
+                    <dt><a href="">&nbsp;</a></dt>
+                    <dd>&nbsp;</dd>    
+                </li>
+                <li>
+                    <a href="">
+                        <div class="recommend_content_list_node">
+                            <div class="recommend_number"><p>2</p></div>
+                        </div>
+                    </a>
+                    <dt><a href="">&nbsp;</a></dt>
+                    <dd>&nbsp;</dd>    
+                </li>
+                <li>
+                    <a href="">
+                        <div class="recommend_content_list_node">
+                            <div class="recommend_number"><p>3</p></div>
+                        </div>
+                    </a>
+                    <dt><a href="">&nbsp;</a></dt>
+                    <dd>&nbsp;</dd>    
+                </li>
+            </ul>
+        </div>`;
+    }
+}
+Fling.TileView = class extends Fling.View {
+    constructor(params) {
+        super();
+        this.data = params;
+        console.info(this.data);
+    }
+}
+Fling.SlideView = class extends Fling.View {
+    constructor() {
+        super(params);
+    }
+}
+Fling.Section = class {
+    constructor(params, viewObject) {
+        this.title = params.title;
+        this.id = params.id;
+        this.view = viewObject;
+        console.info(this.view);
+    }
+
+    get render() {
+        let data = {
+            id: this.id,
+            title: this.title
+        }
+        let source = `
+        <section class="main_section content {{id}}" id="{{id}}">
+            <div class="section_title">{{title}}</div>
+            ${this.view.render}
+        </section>`
+
+        let template = Handlebars.compile(source);
+        return template(data);
+    }
+}
+
 document.addEventListener("DOMContentLoaded", (e) => {
     XHR.get('http://52.79.119.41/recipes', (e) => {
         const data = JSON.parse(e.target.responseText);
         fillContentRecommendSection(data, ".recommend_content_list");
+        var rankSection = new Fling.Section({
+            title: '플링 인기 차트',
+            id: 'recommended'
+        }, new Fling.TileView(data));
+        document.querySelector('.container').insertAdjacentHTML('beforeend', rankSection.render);
+        console.info(rankSection.render);
     });
     XHR.get('http://52.79.119.41/season', (e) => {
         const data = JSON.parse(e.target.responseText);
