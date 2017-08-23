0cb14527e2a298058022048f93fba0f9f03b9318
chsbru <chsbru@gmail.com>
2017-08-23 15:52:16 +0900

    메인 페이지 로컬 XHR 함수 제거

--- src/main.js
+++ src/main.js
@@ -1,24 +1,44 @@
 document.addEventListener("DOMContentLoaded", (e) => {
-    _.ajaxFunc("http://52.79.119.41/recipes", fillContentRecommendSection, ".recommend_content_list");
-    _.ajaxFunc("http://52.79.119.41/season", fillContentRecommendSection, ".main_section.season_event .recommend_content_list");
+    XHR.get('http://52.79.119.41/recipes', (e) => {
+        const data = JSON.parse(e.target.responseText);
+        fillContentRecommendSection(data, ".recommend_content_list");
+    });
+    XHR.get('http://52.79.119.41/season', (e) => {
+        const data = JSON.parse(e.target.responseText);
+        fillContentRecommendSection(data, ".main_section.season_event .recommend_content_list");
+    });
+    
     const interval = window.setInterval(fadeInOutMain.bind(this,".main_header_fade_in", ".main_header_fade_out", ".main_header_fade_middle"),6000);
+
+    document.querySelector(".slide_arrow.prev").addEventListener("click", (e) => {
+        const a = document.querySelector(".list_wrap").firstElementChild;
+        let slide_num = a.style.getPropertyValue('--slide-number') * 1;
+        a.style.setProperty('transition', '1s');
+        if (slide_num !== 0)
+            a.style.setProperty('--slide-number', --slide_num);
+        
+    })
+    
+    
+    document.querySelector(".slide_arrow.next").addEventListener("click", (e) => {
+        const a = document.querySelector(".list_wrap").firstElementChild;
+        let slide_num = a.style.getPropertyValue('--slide-number') * 1;
+        a.style.setProperty('transition', '1s');
+        let slide_count;
+        if (window.innerWidth > 1100) {
+            slide_count = 3;
+        }
+        else {
+            slide_count = 9;
+        }
+        if (slide_num + 1 < slide_count)
+            a.style.setProperty('--slide-number', ++slide_num);
+       
+    
+    })
+    
+    
 })
-
-
-
-
-///////////////////////////////////////////// util
-const _ = {
-    ajaxFunc : function ajax(url, exeFunc, selector) {
-    const oReq = new XMLHttpRequest(); 
-        oReq.addEventListener("load", (e) => {
-            const data = JSON.parse(oReq.responseText);
-            exeFunc(data, selector);
-        }); 
-        oReq.open("GET", url); 
-        oReq.send();
-    }
-}
 
 
 //////////////////////////////////////////////////
@@ -67,84 +87,4 @@ function fillContentRecommendSection(data, selector) {
 
 
 /////////////////////////////////////////////////////////////////////////////화살표//////////
-
-document.querySelector(".slide_arrow.prev").addEventListener("click", (e) => {
-    const a = document.querySelector(".list_wrap").firstElementChild;
-    let slide_num = a.style.getPropertyValue('--slide-number') * 1;
-    a.style.setProperty('transition', '1s');
-    if (slide_num !== 0)
-        a.style.setProperty('--slide-number', --slide_num);
-    
-    // //맨 뒤 떼서 앞에 붙이고 제자리
-    // a.addEventListener("transitionend", avs, false);
-
-    // function avs() {
-    //     a.removeEventListener("transitionend", avs);
-    //     console.log(a);
-    //     //const lastNode = a.children[2].cloneNode(true);
-    //     //a.removeChild(a.children[2]);
-    //     //a.insertBefore(lastNode, a.children[0]);
-
-    //     a.removeEventListener("transitionend", avs);
-    //     if (slide_num == 0) {
-    //         a.style.setProperty('transition', 'none');
-    //         let li_arr = Array.from(a.querySelectorAll('li'));
-    //         let sli_lst = li_arr.slice(0, 3);
-            
-    //         for (let i = 2; i >= 0; i--) {
-    //             a.removeChild(sli_lst[i]);
-    //             a.appendChild(sli_lst[i]);
-    //         }
-
-    //         a.style.setProperty('--slide-number', ++slide_num);
-    //     }
-        
-    // }
-    
-})
-
-
-document.querySelector(".slide_arrow.next").addEventListener("click", (e) => {
-    const a = document.querySelector(".list_wrap").firstElementChild;
-    let slide_num = a.style.getPropertyValue('--slide-number') * 1;
-    a.style.setProperty('transition', '1s');
-    let slide_count;
-    if (window.innerWidth > 1100) {
-        slide_count = 3;
-    }
-    else {
-        slide_count = 9;
-    }
-    if (slide_num + 1 < slide_count)
-        a.style.setProperty('--slide-number', ++slide_num);
-    // a.addEventListener("transitionend", avs, false);
-
-    // function avs() {
-    //     a.style.removeProperty('transition');
-    //     a.removeEventListener("transitionend", avs);
-    //     if (slide_num == 2) {
-    //         a.style.setProperty('transition', 'none');
-    //         let li_arr = Array.from(a.querySelectorAll('li'));
-    //         let firstNode = li_arr[0].cloneNode(true);
-    //         let sli_lst = li_arr.slice(0,3);
-            
-    //         for (let item of sli_lst) {
-    //             a.removeChild(item);
-    //             console.info(li_arr[0]);
-    //             a.insertBefore(item, a.querySelectorAll('li').lastNode);
-    //         }
-    //         a.style.setProperty('--slide-number', --slide_num);
-    //     }
-        
-
-    //     return;
-    //     //const firstNode = a.children[0].cloneNode(true);
-    //     a.removeChild(a.children[0]);
-    //     a.insertBefore(firstNode, a.children[2]);
-    //     a.style.transition = "none";
-    //     a.style.transform = "translateX(0px)";
-    //     a.removeEventListener("transitionend", avs);
-    // }
-
-})
 
